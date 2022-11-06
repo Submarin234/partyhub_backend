5ec4427e34713dd4e7ca941b60a68e683e3aabe8
bobert7 <robert.ieremie@gmail.com>
2022-11-06 13:01:03 +0200

    initial commit

---
+++ src/main/java/com/submarin/partyhub/PartyHubApplication.java
@@ -0,0 +1,14 @@
+package com.submarin.partyhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartyHubApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(PartyHubApplication.class, args);
+    }
+
+}
