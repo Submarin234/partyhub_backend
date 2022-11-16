fa5c44c4b2bd4822234b05a88efb4cf47564552f
Adrian1444 <adrian.lungu.mihai@gmail.com>
2022-11-16 20:17:09 +0200

    Event Controller

--- src/main/java/com/submarin/partyhub/PartyHubApplication.java
+++ src/main/java/com/submarin/partyhub/PartyHubApplication.java
@@ -2,8 +2,11 @@ package com.submarin.partyhub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+// TODO: 11/16/2022
+//taie exclude cand e configurata baza de date
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class PartyHubApplication {
 
 
